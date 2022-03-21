3fc3ad22eb27bfa2909d261db8d2dd639bf20890
Emil Vinther Lorentzen <emil380e@zbc.dk>
2022-03-21 13:50:09 +0100

    Done

--- Age/Program.cs
+++ Age/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Console.WriteLine("Write your name");
+string name = Console.ReadLine();
+
+Console.WriteLine("How old are you?");
+int age = int.Parse(Console.ReadLine());
+
+if (age < 3)
+{
+    Console.WriteLine("{0} du må gå med ble", name);
+}
+else if (age > 3 && age <= 15)
+{
+    Console.WriteLine("{0}, du må ingenting", name);
+}
+else if (age > 15 && age < 18)
+{
+    Console.WriteLine("{0}, du må drikke", name);
+}
+else Console.WriteLine("{0}, du må stemme og køre bil", name);
+
+Console.ReadKey();
+
